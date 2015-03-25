be13d97ceca35f268ba20c6ae9bbeac10867cd4c
Sam Wilson <sam@samwilson.id.au>
2015-03-25 12:29:41 +0800

    Allow modules to provide Artisan commands.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,28 +1,29 @@
 <?php namespace Ormic\Console;
 
-use Illuminate\Console\Scheduling\Schedule;
-use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Events\Dispatcher;
 
-class Kernel extends ConsoleKernel
+class Kernel extends \Illuminate\Foundation\Console\Kernel
 {
 
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
+    /** @var array List of Artisan commands. */
     protected $commands = [
         'Ormic\Console\Commands\Upgrade',
     ];
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-//	protected function schedule(Schedule $schedule)
-//	{
-//		$schedule->command('inspire')
-//				 ->hourly();
-//	}
+
+    public function __construct(Application $app, Dispatcher $events)
+    {
+        parent::__construct($app, $events);
+        $fs = new Filesystem();
+        $modules = new \Ormic\Modules();
+        foreach ($modules->getAll() as $name => $path) {
+            $commandsDir = app_path() . '/../' . $path . '/Console/Commands';
+            $commandFiles = $fs->files($commandsDir);
+            foreach ($commandFiles as $commandFile) {
+                $command = substr(basename($commandFile), 0, -4);
+                $this->commands[] = "Ormic\\modules\\$name\\Console\\Commands\\$command";
+            }
+        }
+    }
 }
